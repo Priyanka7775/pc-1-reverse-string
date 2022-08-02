fc10f78ff50194115376d5aae7d1cbb319e18558
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2022-08-02 10:44:38 +0530

    Sprint 5

--- src/main/java/com/jap/ReverseStringDemo.java
+++ src/main/java/com/jap/ReverseStringDemo.java
@@ -3,19 +3,27 @@ package com.jap;
 public class ReverseStringDemo {
     public static void main(String[] args) {
 
-
-
         //Create a String object.
+        String string="Programming";
 
         //Create an object of the class ReverseStringDemo
+        ReverseStringDemo r=new ReverseStringDemo();
 
         //Call the method getReverseString() and pass the parameter
+        r.getReverseString(string);
 
 
     }
 //Write the logic to reverse the String  inside the below method and return the reverse String.
 public String getReverseString(String string){
 
-        return "";
+        char ch[]=string.toCharArray();
+        String rev="";
+        for(int i=ch.length-1;i>=0;i--){
+            rev+=ch[i];
+
+        }
+        System.out.println("Reverse string is :" +rev);
+        return rev;
     }
 }
